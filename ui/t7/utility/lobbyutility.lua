28fb2bce04a8472c0a879ff9397ff14ed13300fc
Michael Akopyan <michaelakopyan29@yahoo.com>
2018-03-31 16:37:36 -0700

    misc

--- ui/t7/utility/lobbyutility.lua
+++ ui/t7/utility/lobbyutility.lua
@@ -1,9 +1,9 @@
 require("ui.t7.utility.lobbyutilityog") -- Ripped original file from Wraith
 
 function Engine.GetLobbyMaxClients()
-    Engine.SetDvar("sv_maxclients", 18)
-    Engine.SetDvar("com_maxclients", 18)
-    Engine.SetLobbyMaxClients(Enum.LobbyType.LOBBY_TYPE_GAME, 18)
-    Engine.SetLobbyMaxClients(Enum.LobbyType.LOBBY_TYPE_PRIVATE, 18)
-    return 18
+    Engine.SetDvar("sv_maxclients", 8)
+    Engine.SetDvar("com_maxclients", 8)
+    Engine.SetLobbyMaxClients(Enum.LobbyType.LOBBY_TYPE_GAME, 8)
+    Engine.SetLobbyMaxClients(Enum.LobbyType.LOBBY_TYPE_PRIVATE, 8)
+    return 8
 end
